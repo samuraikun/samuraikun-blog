b0fc8a6ef3846e9cb37d4d7a779dca6899d9fba5
samuraikun <tazitaziawawa@gmail.com>
2019-12-27 17:44:51 +0900

    fix next.config for deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
+require("dotenv").config();
 const Dotenv = require("dotenv-webpack");
+const axios = require("axios");
 
 module.exports = {
   webpack: config => {
@@ -15,5 +17,27 @@ module.exports = {
       })
     ];
     return config;
+  },
+  exportPathMap: async function() {
+    const paths = {
+      "/": { page: "/" }
+    };
+    const key = {
+      headers: { "X-API-KEY": process.env.API_KEY }
+    };
+    const res = await axios.get(
+      `https://samuraikun.microcms.io/api/v1/blogs`,
+      key
+    );
+    const data = await res.data.contents;
+
+    data.forEach(blog => {
+      paths[`/blogs/${blog.id}`] = {
+        page: "/blogs/[id]",
+        query: { id: blog.id }
+      };
+    });
+
+    return paths;
   }
 };
