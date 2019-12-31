ecbca2b6689ee48e092fb4c759b54ccfdfff7f01
samuraikun <tazitaziawawa@gmail.com>
2019-12-31 16:05:57 +0900

    fix AppHeader

--- src/components/molecules/AppHeader/index.tsx
+++ src/components/molecules/AppHeader/index.tsx
@@ -1,11 +1,24 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import Link from "next/link";
+import Head from "next/head";
 import styled from "styled-components";
 
 const AppHeader: FC = () => (
-  <Link href={"/"}>
-    <HeaderItem>{"Samuraikun's trace log"}</HeaderItem>
-  </Link>
+  <Fragment>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <title>{"Samuraikun's trace log"}</title>
+      <link
+        href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+        rel="stylesheet"
+      />
+      <link rel="shortcut icon" href="/public/favicon.ico" />
+    </Head>
+    <Link href={"/"}>
+      <HeaderItem>{"Samuraikun's trace log"}</HeaderItem>
+    </Link>
+  </Fragment>
 );
 
 const HeaderItem = styled.a`
